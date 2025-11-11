2274bdee04aa3e169d33c539b0ee44b60230a72a
FubarP <jwmpine@gmail.com>
2025-11-11 16:06:41 -0600

    Added Buttons for equipping Magazine.

--- magazine/setup/item.lua
+++ magazine/setup/item.lua
@@ -1,12 +1,27 @@
 Magazines = {
-    ['magazine'] = {
-        label = 'Magazine',
-        consume = 0,
-        weight = 20,
-        stack = false, -- each mag is unique
-        client = {
-            export = 'p_ox_inventory_addon.useMagazine',
-        },
-        magazine = true,
-    },
+	['magazine'] = {
+		label = 'Magazine',
+		consume = 0,
+		weight = 20,
+		stack = false, -- each mag is unique
+		client = {
+			export = 'p_ox_inventory_addon.useMagazine',
+		},
+		buttons = {
+			{
+				label = 'Equip & Pack',
+				action = function(slot)
+					local item = PlayerData.inventory[slot]
+					exports.p_ox_inventory_addon:equipMagazine({name = item.name, slot = item.slot, metadata = item.metadata})
+				end
+			},
+			{
+				label = 'Unequip Magazine',
+				action = function(slot)
+					exports.p_ox_inventory_addon:unequipMagazine()
+				end
+			},
+		},
+		magazine = true,
+	},
 }
